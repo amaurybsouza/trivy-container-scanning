7c021c2454468aa4ba07b0b89194711ff5b66572
amaurybsouza <amaurybsouza@gmail.com>
2025-08-15 20:49:59 -0300

    add s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,3 +38,12 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
